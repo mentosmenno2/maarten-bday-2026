1865eea4f26dee8f64778aac7bb2cef3a6ad4f90
menno-ll <menno@level-level.com>
2026-09-01 20:45:54 +0200

    fullscreen

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,1 @@
 import './styles/main.scss';
-
-const appElement = document.querySelector<HTMLDivElement>('#app');
-
-if (appElement) {
-    appElement.textContent = 'Windows XP interface — setup werkt.';
-}
